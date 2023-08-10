b6e7eddbb4a2b6c05776a8f235c6d3e16bc0da25
MKarthik1729 <karthik.m20@iiits.in>
2023-08-10 12:44:28 +0530

    abhi kar folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-
+import Abhi from './Abhi/Abhi';
+import Karthi from './Karthik/Karthi';
+import Home from './Home';
 function App() {
   return (
-    <div >
-
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/abhi" element={<Abhi />} />
+        <Route path="/karthik" element={<Karthi />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Karthik/Karthi.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Karthi() {
+  return (
+    <div className='App'>Karthgrqk24pok5tqik[oi</div>
+  )
+}
+
+export default Karthi
